b6dba1758323128ab23125cb9ab3ddcab670bf0c
beluzzi <david.leibensperger.main@gmail.com>
2023-09-26 19:37:50 +0200

    reverted to xorg cuz holy shit fuck wayland

---
+++ i3desktop/.config/i3/screens.sh
@@ -0,0 +1,2 @@
+#!/bin/sh
+xrandr --output DVI-D-1 --off --output DP-1 --primary --mode 2560x1440 --pos 1080x240 --rotate normal --output DP-2 --mode 1920x1080 --pos 0x0 --rotate left --output HDMI-1 --off --output HDMI-2 --mode 1920x1080 --pos 3640x420 --rotate normal
